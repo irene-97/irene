0c226c3bfb1aed4b2c40114dabbc7a90ae796cb5
Irene Artemova <artemova.irene@gmail.com>
2017-10-13 02:21:38 +0400

    Правки мобильного меню

--- public/includes/block-header.php
+++ public/includes/block-header.php
@@ -1,47 +1,47 @@
 <header>
-    <div class="container">
+	<div class="container">
         <span>
             <img src="images/svg/avatar.svg" alt="">
             Ирина Артемова
         </span>
-        <div class="navigation">
-            <a href="#skills" class="js-scrollTo">
-                Навыки
-            </a>
-            <a href="#works" class="js-scrollTo">
-                Работы
-            </a>
-            <a href="#contacts" class="js-scrollTo">
-                Контакты
-            </a>
+		<div class="navigation">
+			<a href="#skills" class="js-scrollTo">
+				Навыки
+			</a>
+			<a href="#works" class="js-scrollTo">
+				Работы
+			</a>
+			<a href="#contacts" class="js-scrollTo">
+				Контакты
+			</a>
 			<a href="https://ireneblog.beliarh.net/" target="_blank">
 				Блог
 			</a>
-        </div>
-        <button class="humburger">
-            <img src="/images/svg/humburger.svg" alt="Меню">
-        </button>
-        <div class="navigation-small">
-            <a href="#skills" class="js-scrollTo">
-                Навыки
-            </a>
-            <a href="#works" class="js-scrollTo">
-                Работы
-            </a>
-            <a href="#contacts" class="js-scrollTo">
-                Контакты
-            </a>
+		</div>
+		<button class="humburger">
+			<img src="/images/svg/humburger.svg" alt="Меню">
+		</button>
+		<div class="navigation-small">
+			<a href="#skills" class="js-scrollTo">
+				Навыки
+			</a>
+			<a href="#works" class="js-scrollTo">
+				Работы
+			</a>
+			<a href="#contacts" class="js-scrollTo">
+				Контакты
+			</a>
 			<a href="https://ireneblog.beliarh.net/" target="_blank">
 				Блог
 			</a>
-        </div>
-        <div class="text">
-            Front End Developer<br>
-            &<br>
-            Web Designer
-        </div>
-        <div class="arrow wow bounce" data-wow-duration="5s" data-wow-iteration="3">
-            <img src="images/svg/arrow-down-sign-to-navigate.svg" alt="Вниз">
-        </div>
-    </div>
+		</div>
+		<div class="text">
+			Front End Developer<br>
+			&<br>
+			Web Designer
+		</div>
+		<div class="arrow wow bounce" data-wow-duration="5s" data-wow-iteration="3">
+			<img src="images/svg/arrow-down-sign-to-navigate.svg" alt="Вниз">
+		</div>
+	</div>
 </header>
