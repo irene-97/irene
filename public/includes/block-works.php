9a1f4ce67a4f1468dfd347c5ca0b186695117ff2
Irene Artemova <artemova.irene@gmail.com>
2017-10-12 19:40:19 +0400

    Добавила работу. Прописала alt к работам

--- public/includes/block-works.php
+++ public/includes/block-works.php
@@ -5,93 +5,99 @@
         </h2>
         <div class="works">
             <a class="work" href="https://premia.beliarh.net/" target="_blank" title="2016">
-                <img src="/images/works/premia.jpg" alt="">
+                <img src="/images/works/premia.jpg" alt="Премия конкуретные закупки">
                 <p>
                     Вёрстка сайта «Премия конкурентных закупок»
                 </p>
             </a>
             <a class="work" href="https://birtrans.beliarh.net/" target="_blank" title="2016">
-                <img src="/images/works/birtrans.jpg" alt="">
+                <img src="/images/works/birtrans.jpg" alt="Биртранс">
                 <p>
                     Вёрстка сайта компании «Биртранс»
                 </p>
             </a>
             <a class="work" href="https://domchehov.beliarh.net/" target="_blank" title="2016">
-                <img src="/images/works/domchehov.jpg" alt="">
+                <img src="/images/works/domchehov.jpg" alt="МДЧ">
                 <p>
                     Вёрстка сайта мебельного магазина «МДЧ»
                 </p>
             </a>
             <a class="work" href="https://pizza.beliarh.net/" target="_blank" title="2017">
-                <img src="/images/works/pizza.jpg" alt="">
+                <img src="/images/works/pizza.jpg" alt="Доставка пиццы">
                 <p>
                     Дизайн, вёрстка и программирование лендинга доставки пиццы
                 </p>
             </a>
             <a class="work" href="https://yazykov.beliarh.net/" target="_blank" title="2017">
-                <img src="/images/works/yazykov.jpg" alt="">
+                <img src="/images/works/yazykov.jpg" alt="Поэт Языков">
                 <p>
                     Дизайн, вёрстка и программирование сайта «Поэт Н.М.Языков»
                 </p>
             </a>
             <a class="work" href="https://cookery.beliarh.net/" target="_blank" title="2017">
-                <img src="/images/works/cookery.jpg" alt="">
+                <img src="/images/works/cookery.jpg" alt="Кулинарные рецепты">
                 <p>
                     Дизайн, вёрстка и программирование сайта «Кулинарные рецепты»
                 </p>
             </a>
             <a class="work" href="https://advertany.beliarh.net/" target="_blank" title="2017">
-                <img src="/images/works/advertany.jpg" alt="">
+                <img src="/images/works/advertany.jpg" alt="Доска объявлений">
                 <p>
                     Дизайн, вёрстка и программирование доски объявлений
                 </p>
             </a>
             <a class="work" href="https://irene.beliarh.net/" target="_blank" title="2017">
-                <img src="/images/works/irene.beliarh.jpg" alt="">
+                <img src="/images/works/irene.beliarh.jpg" alt="Портфолио Ирины Артемовой">
                 <p>
                     Дизайн и вёрстка портфолио веб-разработчика
                 </p>
             </a>
             <a class="work" href="/images/works/Cafe-главная.jpg" target="_blank" title="2017">
-                <img src="/images/works/Cafe.jpg" alt="">
+                <img src="/images/works/Cafe.jpg" alt="Кофейня">
                 <p>
                     Дизайн сайта кофейни
                 </p>
             </a>
             <a class="work" href="/images/works/Shopbear-главная.jpg" target="_blank" title="2017">
-                <img src="/images/works/Shopbear.jpg" alt="">
+                <img src="/images/works/Shopbear.jpg" alt="Магазин мишек">
                 <p>
                     Дизайн сайта магазина игрушек
                 </p>
             </a>
             <a class="work" href="/images/works/Ветклиника-главная.jpg" target="_blank" title="2017">
-                <img src="/images/works/Ветклиника.jpg" alt="">
+                <img src="/images/works/Ветклиника.jpg" alt="Ветклиника">
                 <p>
                     Дизайн сайта ветклиники
                 </p>
             </a>
             <a class="work" href="/images/works/Портфолио-главная.jpg" target="_blank" title="2017">
-                <img src="/images/works/Портфолио.jpg" alt="">
+                <img src="/images/works/Портфолио.jpg" alt="Портфолио Ирины Артемовой">
                 <p>
                     Дизайн сайта-портфолио
                 </p>
             </a>
             <a class="work" href="/images/works/Суши-роллы-презентация.jpg" target="_blank" title="2017">
-                <img src="/images/works/Суши-роллы.jpg" alt="">
+                <img src="/images/works/Суши-роллы.jpg" alt="Доставка суши и роллов">
                 <p>
                     Дизайн сайта доставки суши и роллов
                 </p>
             </a>
             <a class="work" href="/images/works/Участки.jpg" target="_blank" title="2017">
-                <img src="/images/works/СуперДача.jpg" alt="">
+                <img src="/images/works/СуперДача.jpg" alt="Супер ДАЧА">
                 <p>
                     Дизайн сайта «СуперДАЧА» (тестовое задание)
                 </p>
             </a>
             <a class="work" href="https://irene-97.github.io/mobile-app/" target="_blank" title="2017">
-                <img src="/images/works/mobileApp.jpg" alt="">
+                <img src="/images/works/mobileApp.jpg" alt="MobileApp">
                 <p>
                     Верстка сайта «MobileApp»
+                </p>
+            </a>
+            <a class="work" href="https://irene-97.github.io/strict/" target="_blank" title="2017">
+                <img src="/images/works/strict.jpg" alt="Strict">
+                <p>
+                    Верстка сайта «Strict»
                 </p>
             </a>
         </div>
