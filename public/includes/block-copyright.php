5cb1efd07a31bac68d4e476db9e4c7acc50ceb5d
Irene Artemova <artemova.irene@gmail.com>
2017-10-26 21:23:27 +0400

    Сделала "пасхалку"

--- public/includes/block-copyright.php
+++ public/includes/block-copyright.php
@@ -4,4 +4,7 @@
             © Ирина Артемова | Веб-разработка | г. Ульяновск, <?= date('o') ?>
         </h3>
     </div>
+	<div class="cat">
+		<img src="images/svg/Cat.svg" alt="Irene Artemova">
+	</div>
 </div>
