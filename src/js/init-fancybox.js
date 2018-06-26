736707cac972e4421e09888d29c983b303a36627
Irene Artemova <artemova.irene@gmail.com>
2018-06-26 15:41:04 +0400

    Добавлены подсказки на русском для fancybox

---
+++ src/js/init-fancybox.js
@@ -0,0 +1,26 @@
+$.fancybox.defaults.tpl.error = '<p class="fancybox-error">Содержимое не может быть загружено.<br/>Пожалуйста, повторите запрос позже.</p>';
+$.fancybox.defaults.tpl.closeBtn = '<a title="Закрыть" class="fancybox-item fancybox-close" href="javascript:;"></a>';
+$.fancybox.defaults.tpl.next = '<a title="Вперед" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>';
+$.fancybox.defaults.tpl.prev = '<a title="Назад" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>';
+
+$.fancybox.helpers.buttons.defaults.tpl = `
+    <div id="fancybox-buttons">
+        <ul>
+            <li>
+                <a class="btnPrev" title="Назад" href="javascript:;"></a>
+            </li>
+            <li>
+                <a class="btnPlay" title="Слайдшоу" href="javascript:;"></a>
+            </li>
+            <li>
+                <a class="btnNext" title="Вперед" href="javascript:;"></a>
+            </li>
+            <li>
+                <a class="btnToggle" title="Изменить размер" href="javascript:;"></a>
+            </li>
+            <li>
+                <a class="btnClose" title="Закрыть" href="javascript:;"></a>
+            </li>
+        </ul>
+    </div>
+`;
--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ svg4everybody();
 // @include('./wow.js')
 // @include('./more.js')
 // @include('./fancybox.js')
+// @include('./init-fancybox.js')
 
 $('h2 .info').on('click', () => {
 	$('h2').toggleClass('info-text-open');
