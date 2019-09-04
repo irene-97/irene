66fec97ceb91b7dfa54883fed028d872cc094940
Irene Artemova <artemova.irene@gmail.com>
2019-09-04 15:05:15 +0400

    Убрана лишняя фигня

--- src/components/Copyright/index.js
+++ src/components/Copyright/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './index.scss';
 
-import cat from './images/cat.svg';
-
 export default class Copyright extends React.Component {
 	render() {
 		const currentYear = new Date().getFullYear();
@@ -11,9 +9,6 @@ export default class Copyright extends React.Component {
 			<div className="copyright">
 				<div className="container">
 					<h3>© Ирина Артемова, г. Ульяновск, {currentYear}</h3>
-				</div>
-				<div className="cat">
-					<img src={cat} alt="Irene Artemova"/>
 				</div>
 			</div>
 		);
